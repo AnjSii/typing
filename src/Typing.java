0a57f51caf3b2ca210ea58b1cf64434d13fca126
Xun Wu <wuxun@tomowork.com>
2018-07-26 16:28:26 +0800

    typing-03 相似度是用%

--- src/Typing.java
+++ src/Typing.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.NumberFormat;
 import java.util.Date;
 
 import javax.swing.*;
@@ -138,7 +139,7 @@ public class Typing {
 				"结果", JOptionPane.INFORMATION_MESSAGE);
 	}
 
-	private float result() {
+	private String result() {
 		String userArticle = textArea.getText();
 		String contrastArticle = "  I am the victim of an internet scam. It is very hard to write that sentence, but it's necessary in order to warn my fellow clowns, magicians and other entertainers, and to prevent them from being taken for $2,800.00 like I was. Here is what happened to me, and how you can avoid it.\n" +
 				"  I run the clown-ministry.com web site, and I perform as a clown at both ministry and secular events. Due to the popularity of the clown-ministry.com web site (do a search on Google .com for 'clown' and it's the 7th site listed, for example) I'm easily found by people -- for both good and bad reasons. For instance, last year my wife Ellen and I taught clowning for a week at the Seventh Day Adventists' national Camporee -- we're not Adventists, and don't know many people who are, but we were found by them & hired primarily due to the popularity of the web site. We've been invited to perform in clown at a conference in Canada, performed at company picnics, etc. well out of our neck of the wood, and so on. Please understand that this isn't bragging; it's setting the stage for what happened in the scam.\n" +
@@ -150,7 +151,12 @@ public class Typing {
 				"  Like an idiot, I said \"yes.\" The check had been deposited and cleared (I thought), and I didn't want to hold onto money that didn't belong to me. So, I went to the local Western Union office and sent $2,300.00 to an individual in the United Kingdom. This didn't set off any red flags (although it should have) since it seemed like the type of silly mistake I've seen people do before. What happened next, though, set off every red flag in my head, and then some.\n" +
 				"  Days later, I received another email from the man supposedly from Cincinnati. According to him, his mother had an advanced form of lung cancer, and needed an immediate operation. He wanted me to send the remaining money to his mother's doctor in Nigeria and he would reschedule the party later. When I heard the word 'Nigeria' I had icicles running up and down my spine. Nigeria is famous, in the internet world, for being Home to the most prolific scam of all time. You've likely received spam email about it. In a nutshell, it involves the widow of some minister or rich person, who is trying to smuggle their immense fortune out of the country before it's seized by her dead husband's adversaries; in exchange for letting them use your bank account, they will give you (various amounts of money, if not enough to make you filthy rich, at least enough to make you dingy\n";
 		EditDistanceCalculator editDistanceCalculator = new EditDistanceCalculator();
-		return editDistanceCalculator.correct(contrastArticle, userArticle);
+		float similarity =  editDistanceCalculator.correct(contrastArticle, userArticle);
+
+		NumberFormat numberFormat = NumberFormat.getPercentInstance();
+		numberFormat.setMaximumFractionDigits(5);
+
+		return numberFormat.format(similarity);
 	}
 
 	private void resultsBtnAction() {
